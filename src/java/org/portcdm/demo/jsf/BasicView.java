84dc8611a62245ab1a773e718066f687cc8b55ca
Glenn <Glenn@10.170.20.212>
2014-08-06 09:11:16 +0200

    First simple demo that present a static tree structure.

---
+++ src/java/org/portcdm/demo/jsf/BasicView.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.portcdm.demo.jsf;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
+/**
+ *
+ * @author Glenn
+ */
+@Named(value = "basicView")
+@ViewScoped
+public class BasicView {
+
+    private String hello = "Hello World";
+    private TreeNode root;
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public BasicView() {
+    }
+
+    @PostConstruct
+    public void init() {
+        root = new DefaultTreeNode("Root", null);
+        TreeNode node0 = new DefaultTreeNode("Node 0", root);
+        TreeNode node1 = new DefaultTreeNode("Node 1", root);
+
+        TreeNode node00 = new DefaultTreeNode("Node 0.0", node0);
+        TreeNode node01 = new DefaultTreeNode("Node 0.1", node0);
+
+        TreeNode node10 = new DefaultTreeNode("Node 1.0", node1);
+
+        node1.getChildren().add(new DefaultTreeNode("Node 1.1"));
+        node00.getChildren().add(new DefaultTreeNode("Node 0.0.0"));
+        node00.getChildren().add(new DefaultTreeNode("Node 0.0.1"));
+        node01.getChildren().add(new DefaultTreeNode("Node 0.1.0"));
+        node10.getChildren().add(new DefaultTreeNode("Node 1.0.0"));
+        root.getChildren().add(new DefaultTreeNode("Node 2"));
+    }
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+}
